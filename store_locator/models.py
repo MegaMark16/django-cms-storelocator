2f9f208d4a5eae6d7b4a3d6f128bf42001977039
ygneo <antonio.barcia@gmail.com>
2013-04-20 22:26:50 +0200

    Use Google Maps API v3 and add a higher level of zoom

--- store_locator/models.py
+++ store_locator/models.py
@@ -15,7 +15,7 @@ class LocationManager(models.Manager):
         queryset = super(LocationManager, self).get_query_set()
         rough_distance = distance / 2
         queryset = queryset.filter(
-                    latitude__range=(source_latitude - rough_distance, source_latitude + rough_distance), 
+                    latitude__range=(source_latitude - rough_distance, source_latitude + rough_distance),
                     longitude__range=(source_longitude - rough_distance, source_longitude + rough_distance)
                     )
         locations = []
@@ -29,7 +29,7 @@ class LocationManager(models.Manager):
                     setattr(location, 'distance', exact_distance)
                     locations.append(location)
                     #print "%s - %s" % (location, exact_distance)
-                    
+
         return locations
         #queryset = queryset.filter(id__in=[l.id for l in locations])
         #return queryset
@@ -37,10 +37,10 @@ class LocationManager(models.Manager):
     def GetDistance(self, source_latitude, source_longitude, target_location):
         lat_1 = math.radians(source_latitude)
         long_1 = math.radians(source_longitude)
-        
+
         lat_2 = math.radians(target_location.latitude)
         long_2 = math.radians(target_location.longitude)
-        
+
         dlong = long_2 - long_1
         dlat = lat_2 - lat_1
         a = (math.sin(dlat / 2))**2 + math.cos(lat_1) * math.cos(lat_2) * (math.sin(dlong / 2))**2
@@ -64,7 +64,7 @@ class Location(models.Model):
     description = models.TextField(blank=True, null=True)
     phone = models.CharField(max_length=255, blank=True, null=True)
     url = models.URLField(max_length=255, blank=True, null=True)
-    
+
     objects = LocationManager()
 
     class Meta:
@@ -73,19 +73,20 @@ class Location(models.Model):
 
     def __unicode__(self):
         return self.name
-    
+
     def get_single_line_address(self):
         return self.address.replace('\n', ', ')
 
 DISTANCE_CHOICES = (
-    ('1', '1 Miles'),        
-    ('5', '5 Miles'),        
-    ('10', '10 Miles'),        
-    ('15', '15 Miles'),        
-    ('25', '25 Miles'),        
-    ('50', '50 Miles'),        
-    ('100', '100 Miles'),        
-    ('500', '500 Miles'),        
+    ('1', '1 Miles'),
+    ('5', '5 Miles'),
+    ('10', '10 Miles'),
+    ('15', '15 Miles'),
+    ('25', '25 Miles'),
+    ('50', '50 Miles'),
+    ('100', '100 Miles'),
+    ('500', '500 Miles'),
+    ('2000', '2000 Miles'),
 )
 
 class StoreLocator(CMSPlugin):
